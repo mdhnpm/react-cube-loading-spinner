0107d6c326da9cec3c0eb6beba9f26fba23ee782
mydatahack <thon6thon6@hotmail.com>
2022-02-21 12:22:33 +1100

    Update props - in progress

--- src/CubeLoadingSpinner/CubeLoadingSpinner.tsx
+++ src/CubeLoadingSpinner/CubeLoadingSpinner.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
-import { CssVariables } from './Styles/CssVariables';
-import { Spinner3dFaceGroup } from './Spinner3dFaceGroup';
-import { spinnerAnimationMixin, transformCrossBrosersMixin } from './Styles/Mixins';
-import { spinner3dTopOuterMoveKeyframes, spinnerBottomOuterMove } from './Styles/Keyframes';
+import React from "react";
+import styled from "styled-components";
+import { CssVariables } from "./Styles/CssVariables";
+import { Spinner3dFaceGroup } from "./Spinner3dFaceGroup";
+import {
+  spinnerAnimationMixin,
+  transformCrossBrosersMixin,
+} from "./Styles/Mixins";
+import {
+  spinner3dTopOuterMoveKeyframes,
+  spinnerBottomOuterMove,
+} from "./Styles/Keyframes";
 
 const Spinner3dContainer = styled.div`
   width: var(--spinner-width);
@@ -11,17 +17,37 @@ const Spinner3dContainer = styled.div`
 `;
 
 const Spinner3dTopOuter = styled.div`
-  ${transformCrossBrosersMixin('translateX(var(--cube-offset-x)) translateY(calc(var(--cube-offset-y) * -1))')};
+  ${transformCrossBrosersMixin(
+    "translateX(var(--cube-offset-x)) translateY(calc(var(--cube-offset-y) * -1))"
+  )};
   ${spinnerAnimationMixin(spinner3dTopOuterMoveKeyframes)}
 `;
 
 const Spinner3dBottomOuter = styled.div`
-  ${transformCrossBrosersMixin('translateX(calc(var(--cube-offset-x)) * -1) translateY(var(--cube-offset-y))')};
+  ${transformCrossBrosersMixin(
+    "translateX(calc(var(--cube-offset-x)) * -1) translateY(var(--cube-offset-y))"
+  )};
   ${spinnerAnimationMixin(spinnerBottomOuterMove)}
-`
+`;
 
-export const CubeLoadingSpinner: React.VFC = () => (
-  <CssVariables>
+interface CubeLoadingSpinnerProps {
+  mainColor?: string;
+  secondaryColor?: string;
+  spinnerWidth?: string;
+  spinnerSpeed?: string;
+}
+export const CubeLoadingSpinner: React.VFC<CubeLoadingSpinnerProps> = ({
+  mainColor = "#141b4d",
+  secondaryColor = "#00c389",
+  spinnerWidth = "32px",
+  spinnerSpeed = "1.5s",
+}) => (
+  <CssVariables
+    mainColor={mainColor}
+    secondaryColor={secondaryColor}
+    spinnerWidth={spinnerWidth}
+    spinnerSpeed={spinnerSpeed}
+  >
     <Spinner3dContainer>
       <Spinner3dTopOuter>
         <Spinner3dFaceGroup />
